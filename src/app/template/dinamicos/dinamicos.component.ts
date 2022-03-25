d0e40d6860ac27f96926e498eaf07e9191682f29
Alucard <xavier_ortega98@hotmail.com>
2022-03-25 00:03:41 -0600

    Template dinamicos terminado

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[]
+}
+
+interface Favorito {
+  id: number,
+  nombre: string
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +16,32 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent   {
 
-  constructor() { }
+  nuevoJuego: string = "";
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Metal',
+    favoritos : [
+      {id: 1, nombre: 'Dark Souls'},
+      {id: 2, nombre: 'Elden Ring'},
+      {id: 3, nombre: 'Gears of War'},
+      {id: 4, nombre: 'Halo Reach'},
+      {id: 5, nombre: 'Batman Arkham City'},
+      {id: 6, nombre: 'Red Dead Redemption 2'}
+    ]
   }
 
+  guardar(){
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego=''
+  }
+
+  borrar(i:number){
+    this.persona.favoritos.splice(i,1);
+  }
 }
