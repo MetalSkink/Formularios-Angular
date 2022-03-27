ac47230a5d1186fcaaf7d410f861157c8b88d089
Alucard <xavier_ortega98@hotmail.com>
2022-03-26 20:18:40 -0600

    Reactivos switches terminado

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent  {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)] ],
+    favoritos: [this.fb.array([
+      ['Arkham city', Validators.required],
+      ['Dragon Ball figtherz',Validators.required]
+    ], Validators.required)]
+  })
 
-  ngOnInit(): void {
+  get favoritosArr(){
+    return this.miFormulario.get('favoritos')?.value as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  campoNoEsValido(campo:string){
+    return this.miFormulario.controls[campo].errors &&
+           this.miFormulario.controls[campo].touched;
+  }
+
+  guardar(){
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return
+    }
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
   }
 
 }
--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  miFormulario:FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue],
+  });
+
+  persona = {
+    genero: 'F',
+    notificaciones: true
+  }
+
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      ...this.persona,
+      condiciones: true
+    });
+
+    this.miFormulario.valueChanges.subscribe(({condiciones,...rest}) => {
+      // delete form.condiciones;
+
+      this.persona= rest;
+    })
+    // suscirbirse a los valores de un campo
+    // this.miFormulario.get('condiciones')?.valueChanges.subscribe(cond => {
+    //   console.log(cond);
+    // })
+  }
+
+  guardar(){
+    const formValue = {...this.miFormulario.value}
+    delete formValue.condiciones;
+
+    this.persona= formValue;
   }
 
 }
